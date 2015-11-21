a089b7dfd14df8f20b8e488d99bea4736c5a7831
Andres Rios <rianby64@gmail.com>
2015-11-21 05:22:40 +0300

    second take for creating test that solve the problem with embedded definitions. Part 1

--- test/gherkin.test.js
+++ test/gherkin.test.js
@@ -10,3 +10,93 @@
  *     to the feature
  *  - do not accept recursive features, scenarios nor steps!
  */
+
+
+/**
+ * One added a Definition must know who is calling it, and when the runner is the caller then its parent is null
+ *  Example:
+ *
+feature('name1', function featureName1() {
+  feature('incorrectEmbeddedFeature1', function incorrectEmbeddedFeature1() { }); // not allowed
+  scenario('name2', function scenarioName2() {
+    feature('incorrectEmbeddedFeature2', function incorrectEmbeddedFeature2() { }); // not allowed
+    scenario('incorrectEmbeddedScenario1', function incorrectEmbeddedScenario1() { }); // not allowed
+    step('name3', function stepName3() {
+      feature('incorrectEmbeddedFeature3', function incorrectEmbeddedFeature3() { }); // not allowed
+      scenario('incorrectEmbeddedScenario2', function incorrectEmbeddedScenario2() { }); // not allowed
+      step('incorrectEmbeddedStep1', function incorrectEmbeddedStep1() { }); // not allowed
+    }); // stepName3.caller = { fn: scenarioName2, type: 'scenario' }
+  }); // scenarioName2.caller = { fn: featureName1, type: 'feature' } // [[this definition can be step too]]
+}); // featureName1.caller = null // [[this definition can be scenario or step too]]
+
+gherkin.definitions = {
+  0: {
+    fn: featureName1,
+    name: 'name1',
+    type: 'feature',
+    definitions: {
+      0: {
+        fn: scenarioName1,
+        name: 'name1',
+        type: 'scenario',
+        definitions: {
+          0: {
+            fn: stepName1,
+            name: 'name1',
+            type: 'step'
+            // definitions is undefined
+          },
+          1: {
+            fn: stepName2,
+            name: 'name2',
+            type: 'step'
+            // definitions is undefined
+          }
+        }
+      },
+      1: {
+        fn: scenarioName1,
+        name: 'name1',
+        type: 'scenario',
+        definitions: {}
+      }
+    }
+  },
+  1: {
+    fn: featureName1,
+    name: 'name2',
+    type: 'feature',
+    definitions: {}
+  }
+} // after running the feature-driven process
+ *
+ */
+describe("The construction of embedded definitions", function() {
+
+  describe("Has an interface that", function() {
+    beforeEach(function() {
+      window.gherkin = new Gherkin();
+    });
+
+    it("contains> feature(name, fn, thisArg)", function() {
+      expect(gherkin.feature instanceof Function).toBe(true);
+      expect(gherkin.feature.length).toBe(3);
+    });
+    it("contains> scenario(name, fn, thisArg)", function() {
+      expect(gherkin.scenario instanceof Function).toBe(true);
+      expect(gherkin.scenario.length).toBe(3);
+    });
+    it("contains> step(name, fn, thisArg)", function() {
+      expect(gherkin.step instanceof Function).toBe(true);
+      expect(gherkin.step.length).toBe(3);
+    });
+
+    it("contains> definitions()", function() {
+      expect(gherkin.definitions instanceof Function).toBe(true);
+      var definitions = gherkin.definitions();
+      expect(definitions instanceof Object).toBe(true);
+    })
+
+    // TODO> check if name, fn and thisArg are regExp|string, function and object|undefined types
+  })
+});
